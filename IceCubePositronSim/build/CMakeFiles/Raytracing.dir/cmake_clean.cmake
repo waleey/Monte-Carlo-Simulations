e6041a9698578578926ad6c5c368b056679f68b8
Waly M Z Karim <walym.zkarim64@gmail.com>
2023-02-10 01:58:36 -0500

    Delete IceCubePositronSim directory

--- IceCubePositronSim/build/CMakeFiles/Raytracing.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Raytracing"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Raytracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
